18c8c68f223aa2c0b6b14a3c6b5e11f7674cdb11
Christian Pschorr <c.pschorr@datamints.com>
2021-09-22 21:42:06 +0200

    Initialer Commit

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,62 @@
+<?php
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Icon Text Module von TT-Content
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+	array(
+		'LLL:EXT:ce_facts/Resources/Private/Language/locallang_tca.xlf:tx_ce_accordeon.title',
+		'ce_accordeon',
+		'ps14-content-accordeon'
+	),
+	'CType',
+	'ce_accordeon'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['ce_accordeon'] = [
+	'showitem' => '
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;xoHeader, bodytext, tx_xo_elements,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+			--palette--;;hidden,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+	',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_accordeon']['columnsOverrides']['bodytext']['config'] = [
+	'enableRichtext' => true,
+	'richtextConfiguration' => 'xoDefault',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_accordeon']['columnsOverrides']['tx_xo_elements']['config']['overrideChildTca'] = [
+	'columns' => [
+		'record_type' => [
+			'config' => [
+				'items' => [
+					['LLL:EXT:ce_facts/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.record_type.default', 'ce_accordeon_default'],
+					['LLL:EXT:ce_facts/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.record_type.records', 'ce_accordeon_records'],
+				],
+				'default' => 'ce_accordeon_default'
+			]
+		]
+	],
+	'types' => [
+		'ce_accordeon_default' => [
+			'showitem' => '
+				l10n_diffsource, record_type, --palette--;;header, description,
+				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;;visibility,
+				--palette--;;access',
+		],
+		'ce_accordeon_records' => [
+			'showitem' => '
+				l10n_diffsource, record_type, --palette--;;header, description,
+				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;;visibility,
+				--palette--;;access',
+		],
+	]
+];
