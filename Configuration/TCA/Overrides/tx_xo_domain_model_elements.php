370bf2da5d2106c9832924c6a119976e59afc0e2
Christian Pschorr <pschorr.christian@gmail.com>
2024-03-16 15:54:25 +0100

    Option (Default) Aktive für Akkordeon-Elemente hinzugefügt

---
+++ Configuration/TCA/Overrides/tx_xo_domain_model_elements.php
@@ -0,0 +1,21 @@
+<?php
+// ---------------------------------------------------------------------------------------------------------------------
+// Weitere Felder in TT-Content
+$tmpAccordionElementsColumns = [
+	'tx_ce_accordion_active' => [
+		'exclude' => true,
+		'l10n_mode' => 'exclude',
+		'label' => 'LLL:EXT:ce_accordion/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.active',
+		'config' => [
+			'type' => 'check',
+			'items' => [
+				'1' => [
+					'0' => 'LLL:EXT:lang/locallang_core.xlf:labels.enabled'
+				]
+			],
+			'default' => 0,
+		]
+	],
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_xo_domain_model_elements', $tmpAccordionElementsColumns);
